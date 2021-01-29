5b220b2f8cebfea940d1f93f330dcadd52c5c7fc
Marcin Badurowicz <m@badurowicz.net>
2021-01-29 13:05:50 +0100

    Dzialajacy prototyp

---
+++ Mesh.cs
@@ -0,0 +1,270 @@
+﻿using ConsoleGameEngine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+
+namespace RexMinus1
+{
+    public struct Triangle
+    {
+        public Vector3[] p;
+
+        public int color;
+        public ConsoleCharacter c;
+
+        public Triangle(Vector3 pa, Vector3 pb, Vector3 pc)
+        {
+            p = new Vector3[3];
+            p[0] = pa;
+            p[1] = pb;
+            p[2] = pc;
+
+            color = 0;
+            c = ConsoleCharacter.Null;
+        }
+
+        public Triangle(object n)
+        {
+            p = new Vector3[3];
+            color = 0;
+            c = ConsoleCharacter.Null;
+        }
+
+        public void Translate(Vector3 delta)
+        {
+            p[0] += delta;
+            p[1] += delta;
+            p[2] += delta;
+        }
+
+        public Triangle MatMul(Matrix m)
+        {
+            Triangle t = new Triangle(null);
+            t.p[0] = Matrix.MultiplyVector(p[0], m);
+            t.p[1] = Matrix.MultiplyVector(p[1], m);
+            t.p[2] = Matrix.MultiplyVector(p[2], m);
+
+            return t;
+        }
+    }
+
+    public struct Mesh
+    {
+        public Vector3[] Vertices { get; set; }
+        public Triangle[] Triangles { get; set; }
+
+        public static Mesh LoadFromObj(string filename)
+        {
+            StreamReader s = new StreamReader(filename);
+
+            List<Vector3> verts = new List<Vector3>();
+            List<Triangle> tris = new List<Triangle>();
+
+            while (!s.EndOfStream)
+            {
+                string line = s.ReadLine();
+
+                if (line[0] == 'v')
+                {
+                    Vector3 v = new Vector3();
+                    string[] str = line.Replace('.', ',').Split();
+                    v.X = float.Parse(str[1]);
+                    v.Y = float.Parse(str[2]);
+                    v.Z = float.Parse(str[3]);
+                    verts.Add(v);
+                }
+                if (line[0] == 'f')
+                {
+                    Triangle t = new Triangle();
+                    string[] str = line.Split();
+                    t.p = new Vector3[3];
+                    t.color = 6;
+
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        if (str[i].Contains("//"))
+                            str[i] = str[i].Substring(0, str[i].IndexOf("//"));
+                    }
+
+                    t.p[0] = verts[Convert.ToInt32(str[1]) - 1];
+                    t.p[1] = verts[Convert.ToInt32(str[2]) - 1];
+                    t.p[2] = verts[Convert.ToInt32(str[3]) - 1];
+                    tris.Add(t);
+                }
+            }
+
+            var result = new Mesh();
+
+            result.Vertices = verts.ToArray();
+            result.Triangles = tris.ToArray();
+
+            return result;
+        }
+    }
+
+    public class Matrix
+    {
+        public float[,] m = new float[4, 4];
+
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            Matrix mat = new Matrix();
+            for (int c = 0; c < 4; c++)
+            {
+                for (int r = 0; r < 4; r++)
+                {
+                    mat.m[r, c] = a.m[r, 0] * b.m[0, c] + a.m[r, 1] * b.m[1, c] + a.m[r, 2] * b.m[2, c] + a.m[r, 3] * b.m[3, c];
+                }
+            }
+            return mat;
+        }
+
+        public static Vector3 MultiplyVector(Vector3 i, Matrix m)
+        {
+            Vector3 v = new Vector3();
+            float w = 0;
+            v.X = i.X * m.m[0, 0] + i.Y * m.m[1, 0] + i.Z * m.m[2, 0] + m.m[3, 0];
+            v.Y = i.X * m.m[0, 1] + i.Y * m.m[1, 1] + i.Z * m.m[2, 1] + m.m[3, 1];
+            v.Z = i.X * m.m[0, 2] + i.Y * m.m[1, 2] + i.Z * m.m[2, 2] + m.m[3, 2];
+            w = i.X * m.m[0, 3] + i.Y * m.m[1, 3] + i.Z * m.m[2, 3] + m.m[3, 3];
+
+            if (w != 0.0f)
+            {
+                v.X /= w; v.Y /= w; v.Z /= w;
+            }
+
+            return v;
+        }
+
+        public static Matrix Translation(Vector3 t)
+        {
+            Matrix mat = new Matrix();
+            mat.m[0, 0] = 1.0f;
+            mat.m[1, 1] = 1.0f;
+            mat.m[2, 2] = 1.0f;
+            mat.m[3, 3] = 1.0f;
+            mat.m[3, 0] = t.X;
+            mat.m[3, 1] = t.Y;
+            mat.m[3, 2] = t.Z;
+            return mat;
+        }
+
+        public static Matrix Identity()
+        {
+            Matrix mat = new Matrix();
+            mat.m[0, 0] = 1.0f; mat.m[0, 1] = 1.0f; mat.m[0, 2] = 1.0f; mat.m[0, 3] = 1.0f;
+            mat.m[1, 0] = 1.0f; mat.m[1, 1] = 1.0f; mat.m[1, 2] = 1.0f; mat.m[1, 3] = 1.0f;
+            mat.m[2, 0] = 1.0f; mat.m[2, 1] = 1.0f; mat.m[2, 2] = 1.0f; mat.m[2, 3] = 1.0f;
+            mat.m[3, 0] = 1.0f; mat.m[3, 1] = 1.0f; mat.m[3, 2] = 1.0f; mat.m[3, 3] = 1.0f;
+            return mat;
+        }
+
+        public static Matrix LookAtLH(Vector3 eye, Vector3 target, Vector3 up)
+        {
+            Vector3 xaxis, yaxis, zaxis;
+            zaxis = Vector3.Subtract(target, eye); zaxis = Vector3.Normalize(zaxis);
+            xaxis = Vector3.Cross(up, zaxis); xaxis = Vector3.Normalize(xaxis);
+            yaxis = Vector3.Cross(zaxis, xaxis);
+
+            Matrix result = Matrix.Identity();
+            result.m[0, 0] = xaxis.X; result.m[1, 0] = xaxis.Y; result.m[2, 0] = xaxis.Z;
+            result.m[0, 1] = yaxis.X; result.m[1, 1] = yaxis.Y; result.m[2, 1] = yaxis.Z;
+            result.m[0, 2] = zaxis.X; result.m[1, 2] = zaxis.Y; result.m[2, 2] = zaxis.Z;
+
+            result.m[3, 0] = Vector3.Dot(xaxis, eye);
+            result.m[3, 1] = Vector3.Dot(yaxis, eye);
+            result.m[3, 2] = Vector3.Dot(zaxis, eye);
+
+            result.m[3, 0] = -result.m[3, 0];
+            result.m[3, 1] = -result.m[3, 1];
+            result.m[3, 2] = -result.m[3, 2];
+
+            return result;
+        }
+
+        public static Matrix PointAtMatrix(Vector3 pos, Vector3 target, Vector3 up)
+        {
+            // ny frammåt
+            Vector3 newForward = target - pos;
+            newForward = Vector3.Normalize(newForward);
+
+            // ny uppåt
+            Vector3 a = newForward * Vector3.Dot(up, newForward);
+            Vector3 newUp = up - a;
+            newUp = Vector3.Normalize(newUp);
+
+            Vector3 newRight = Vector3.Cross(newUp, newForward);
+            Matrix mat = new Matrix();
+            mat.m[0, 0] = newRight.X; mat.m[0, 1] = newRight.Y; mat.m[0, 2] = newRight.Z; mat.m[0, 3] = 0.0f;
+            mat.m[1, 0] = newUp.X; mat.m[1, 1] = newUp.Y; mat.m[1, 2] = newUp.Z; mat.m[1, 3] = 0.0f;
+            mat.m[2, 0] = newForward.X; mat.m[2, 1] = newForward.Y; mat.m[2, 2] = newForward.Z; mat.m[2, 3] = 0.0f;
+            mat.m[3, 0] = pos.X; mat.m[2, 1] = pos.Y; mat.m[2, 2] = pos.Z; mat.m[2, 3] = 1.0f;
+            return mat;
+        }
+
+        public static Matrix ProjectionMatrix(float fov, float aspect, float near, float far)
+        {
+            Matrix mat = new Matrix();
+            float fovRad = 1.0f / (float)Math.Tan(fov * 0.5f / (180 / (float)Math.PI));
+            mat.m[0, 0] = aspect * fovRad;
+            mat.m[1, 1] = fovRad;
+            mat.m[2, 2] = far / (far - near);
+            mat.m[3, 2] = (-far * near) / (far - near);
+            mat.m[2, 3] = 1.0f;
+            mat.m[3, 3] = 0.0f;
+
+            return mat;
+        }
+
+        // Endast för rotation/translations matriser
+        public static Matrix QuickInverse(Matrix m)
+        {
+            Matrix matrix = new Matrix();
+            matrix.m[0, 0] = m.m[0, 0]; matrix.m[0, 1] = m.m[1, 0]; matrix.m[0, 2] = m.m[2, 0]; matrix.m[0, 3] = 0.0f;
+            matrix.m[1, 0] = m.m[0, 1]; matrix.m[1, 1] = m.m[1, 1]; matrix.m[1, 2] = m.m[2, 1]; matrix.m[1, 3] = 0.0f;
+            matrix.m[2, 0] = m.m[0, 2]; matrix.m[2, 1] = m.m[1, 2]; matrix.m[2, 2] = m.m[2, 2]; matrix.m[2, 3] = 0.0f;
+            matrix.m[3, 0] = -(m.m[3, 0] * matrix.m[0, 0] + m.m[3, 1] * matrix.m[1, 0] + m.m[3, 2] * matrix.m[2, 0]);
+            matrix.m[3, 1] = -(m.m[3, 0] * matrix.m[0, 1] + m.m[3, 1] * matrix.m[1, 1] + m.m[3, 2] * matrix.m[2, 1]);
+            matrix.m[3, 2] = -(m.m[3, 0] * matrix.m[0, 2] + m.m[3, 1] * matrix.m[1, 2] + m.m[3, 2] * matrix.m[2, 2]);
+            matrix.m[3, 3] = 1.0f;
+            return matrix;
+        }
+
+        public static Matrix RotationMatrixX(float fAngleRad)
+        {
+            Matrix mat = new Matrix();
+            mat.m[0, 0] = 1.0f;
+            mat.m[1, 1] = (float)Math.Cos(fAngleRad);
+            mat.m[1, 2] = (float)Math.Sin(fAngleRad);
+            mat.m[2, 1] = (float)-Math.Sin(fAngleRad);
+            mat.m[2, 2] = (float)Math.Cos(fAngleRad);
+            mat.m[3, 3] = 1.0f;
+            return mat;
+        }
+
+        public static Matrix RotationMatrixY(float fAngleRad)
+        {
+            Matrix mat = new Matrix();
+            mat.m[0, 0] = (float)Math.Cos(fAngleRad);
+            mat.m[0, 2] = (float)Math.Sin(fAngleRad);
+            mat.m[2, 0] = (float)-Math.Sin(fAngleRad);
+            mat.m[1, 1] = 1.0f;
+            mat.m[2, 2] = (float)Math.Cos(fAngleRad);
+            mat.m[3, 3] = 1.0f;
+            return mat;
+        }
+
+        public static Matrix RotationMatrixZ(float fAngleRad)
+        {
+            Matrix mat = new Matrix();
+            mat.m[0, 0] = (float)Math.Cos(fAngleRad);
+            mat.m[0, 1] = (float)Math.Sin(fAngleRad);
+            mat.m[1, 0] = (float)-Math.Sin(fAngleRad);
+            mat.m[1, 1] = (float)Math.Cos(fAngleRad);
+            mat.m[2, 2] = 1.0f;
+            mat.m[3, 3] = 1.0f;
+            return mat;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,211 @@
+﻿using ConsoleGameEngine;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace RexMinus1
+{
+    internal class Model
+    {
+        public Mesh Mesh { get; set; }
+        public Vector3 Position { get; set; }
+
+        public float RotationY { get; set; }
+        public float RotationX { get; set; }
+    }
+
+    internal class MeshProjection
+    {
+        private readonly ConsoleEngine Engine;
+
+        public List<Model> Models { get; set; }
+
+        public MeshProjection(ConsoleEngine engine)
+        {
+            Models = new List<Model>();
+            lightDirection = Vector3.Normalize(lightDirection);
+
+            CameraPosition = new Vector3(0, 0, -4.0f);
+            LookDirection = new Vector3(0, 0, -1.0f);
+
+            this.consoleHeight = engine.WindowSize.Y;
+            this.consoleWidth = engine.WindowSize.X;
+            this.Engine = engine;
+
+            aspectRatio = (float)consoleHeight / (float)consoleWidth;
+        }
+
+        private readonly int consoleHeight;
+        private readonly int consoleWidth;
+        private readonly float aspectRatio;
+
+        public Vector3 CameraPosition { get; set; }
+        public float CameraRotation { get; set; }
+
+        public Vector3 LookDirection { get; set; }
+        private Vector3 lightDirection = new Vector3(0.0f, 0.0f, 0f);
+
+        private List<Triangle> trianglesToRaster = new List<Triangle>();
+
+        public bool DrawWireframes { get; set; } = false;
+
+        private Matrix projectionMatrix;
+        private Matrix viewMatrix;
+
+        public void UpdateViewMatrix()
+        {
+            viewMatrix = Matrix.LookAtLH(CameraPosition, LookDirection, Vector3.UnitY);
+
+            viewMatrix *= Matrix.RotationMatrixY(CameraRotation);
+
+            float near = 0.1f;
+            float far = 100.0f;
+            float fov = 70.0f;
+
+            projectionMatrix = Matrix.ProjectionMatrix(fov, aspectRatio, near, far);
+            projectionMatrix *= Matrix.RotationMatrixY(CameraRotation);
+        }
+
+        public void UpdateVisibleFaces()
+        {
+            foreach (var model in Models)
+            {
+                var mesh = model.Mesh;
+                var modelPosition = model.Position;
+                var yRot = model.RotationY;
+                var xRot = model.RotationX;
+
+                for (int i = 0; i < mesh.Triangles.Length; i++)
+                {
+                    Triangle vertex = mesh.Triangles[i];
+
+                    var worldMatrix = Matrix.RotationMatrixY(yRot) * Matrix.RotationMatrixX(xRot) *
+                                      Matrix.Translation(modelPosition);
+
+                    var transformMatrix = worldMatrix * viewMatrix * projectionMatrix;
+
+                    Triangle transformed = vertex.MatMul(transformMatrix);
+
+                    Vector3 normal, line1, line2;
+                    line1 = transformed.p[1] - transformed.p[0];
+                    line2 = transformed.p[2] - transformed.p[0];
+
+                    normal = Vector3.Cross(line1, line2);
+                    normal = Vector3.Normalize(normal);
+
+                    if (Vector3.Dot(normal, transformed.p[0] - CameraPosition) < 0.0f)
+                    {
+                        float l = Vector3.Dot(lightDirection, normal);
+
+                        ConsoleCharacter character = ConsoleCharacter.Light;
+                        if (l > 0.4) character = ConsoleCharacter.Medium;
+                        if (l > 0.7) character = ConsoleCharacter.Dark;
+                        if (l > 1) character = ConsoleCharacter.Full;
+
+                        // projekterar från 3D -> 2D
+                        Triangle projected = new Triangle(null);
+                        projected = transformed.MatMul(projectionMatrix);
+
+                        Vector3 offsetView = new Vector3(1, 1, 0);
+                        projected.p[0] += offsetView;
+                        projected.p[1] += offsetView;
+                        projected.p[2] += offsetView;
+
+                        projected.p[0].X *= 0.5f * consoleWidth; projected.p[0].Y *= 0.5f * consoleHeight;
+                        projected.p[1].X *= 0.5f * consoleWidth; projected.p[1].Y *= 0.5f * consoleHeight;
+                        projected.p[2].X *= 0.5f * consoleWidth; projected.p[2].Y *= 0.5f * consoleHeight;
+
+                        projected.c = character;
+                        trianglesToRaster.Add(projected);
+                    }
+                }
+            }
+
+            // sortera
+            trianglesToRaster.Sort((t1, t2) => ((t2.p[0].Z + t2.p[1].Z + t2.p[2].Z).CompareTo((t1.p[0].Z + t1.p[1].Z + t1.p[2].Z))));
+        }
+
+        public void Render()
+        {
+            foreach (Triangle t in trianglesToRaster)
+            {
+                Point a = new Point((int)t.p[0].X, (int)t.p[0].Y);
+                Point b = new Point((int)t.p[1].X, (int)t.p[1].Y);
+                Point c = new Point((int)t.p[2].X, (int)t.p[2].Y);
+
+                if (DrawWireframes)
+                    Engine.Triangle(b, a, c, 9, t.c);
+                else
+                    Engine.Triangle(b, a, c, 10, t.c);
+            }
+
+            Engine.SetPixel(new Point(0, 0), 4);
+
+            trianglesToRaster.Clear();
+        }
+    }
+
+    internal class HelloWorld : ConsoleGame
+    {
+        private MeshProjection meshProjection;
+
+        private Glyph[,] g;
+
+        private static void Main(string[] args)
+        {
+            new HelloWorld().Construct(128, 64, 8, 8, FramerateMode.MaxFps);
+        }
+
+        public override void Create()
+        {
+            Engine.SetPalette(Palettes.Default);
+            TargetFramerate = 30;
+
+            meshProjection = new MeshProjection(Engine);
+            meshProjection.Models.Add(new Model { Mesh = Mesh.LoadFromObj("Assets/Rock2.obj"), Position = new Vector3(0, 0, 2), RotationX = 1.241f });
+            meshProjection.Models.Add(new Model { Mesh = Mesh.LoadFromObj("Assets/Rock2.obj"), Position = new Vector3(4, 0, 2), RotationX = 2.151f });
+            meshProjection.Models.Add(new Model { Mesh = Mesh.LoadFromObj("Assets/Rock2.obj"), Position = new Vector3(-4, 0, 2), RotationX = 0.21f }); ;
+        }
+
+        public override void Update()
+        {
+            meshProjection.Models[0].RotationY += 0.1f;
+            meshProjection.Models[1].RotationY += 0.1f;
+            meshProjection.Models[2].RotationY += 0.1f;
+
+            if (Engine.GetKeyDown(ConsoleKey.LeftArrow))
+            {
+                meshProjection.CameraRotation += 0.05f;
+            }
+
+            if (Engine.GetKeyDown(ConsoleKey.RightArrow))
+            {
+                meshProjection.CameraRotation -= 0.05f;
+            }
+
+            if (Engine.GetKeyDown(ConsoleKey.UpArrow))
+            {
+                float move = 0.1f;
+                meshProjection.CameraPosition = new Vector3(meshProjection.CameraPosition.X, meshProjection.CameraPosition.Y, meshProjection.CameraPosition.Z + move);
+            }
+
+            if (Engine.GetKeyDown(ConsoleKey.DownArrow))
+            {
+                float move = -0.1f;
+                meshProjection.CameraPosition = new Vector3(meshProjection.CameraPosition.X, meshProjection.CameraPosition.Y, meshProjection.CameraPosition.Z + move);
+            }
+
+            meshProjection.UpdateViewMatrix();
+            meshProjection.UpdateVisibleFaces();
+        }
+
+        public override void Render()
+        {
+            Engine.ClearBuffer();
+
+            meshProjection.Render();
+
+            Engine.DisplayBuffer();
+        }
+    }
+}
